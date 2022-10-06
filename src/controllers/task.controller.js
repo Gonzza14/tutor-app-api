75c27c7c429aa6234f99ae12e45b5a96905ea9f1
Gonzza14 <gonzza_@hotmail.com>
2022-10-06 14:36:18 -0600

    operaciones basicas

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -11,11 +11,47 @@ export const findAllTasks = async (req, res) =>{
 //Exportar funcion que crea un registro 
 export const createTask = async (req, res) =>{
     //Crear task
-    const newTask = new Task({title: req.body.title, description: req.body.description})
+    const newTask = new Task({
+        title: req.body.title, 
+        description: req.body.description, 
+        done: req.body.done ? req.body.done : false
+    })
 
     //Guardar en la base de datos
     const taskSaved = await newTask.save();
 
     //ver los datos que el cliente envia al servidor
     res.json(taskSaved)
+}
+
+//Exportar funcion que busca un registro por id
+export const findOneTask = async (req, res) =>{
+    //Buscar por id
+    const task = await Task.findById(req.params.id)
+    //mostrar tarea
+    res.json(task)
+}
+
+//Exportar funcion que elimina un registro
+export const deleteTask = async (req, res) => {
+    //Buscar y borrar por id
+    const data = await Task.findByIdAndDelete(req.params.id)
+    //Mostrar un objeto
+    res.json({
+        message: `${data.title} Task were deleted succesfully`
+    })
+}
+
+//Exportar funcion de tareas hechas
+export const findAllDoneTasks = async(req, res) => {
+      //Trae todas las tareas que esten realizdas
+      const tasks = await Task.find({done : true})
+      res.json(tasks)
+}
+
+//Exportar funcion que actualice 
+export const updateTask = async (req, res) => {
+    //Buscar por id y actualizar 
+    await Task.findByIdAndUpdate(req.params.id, req.body)
+    res.json({message: 'Task was updated succesfully'})
 }
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -9,8 +9,21 @@ const router = Router()
 
 //rutas
 router.get('/', taskController.findAllTasks)
+
+//crear tarea
 router.post('/', taskController.createTask);
 
+//tareas hechas
+router.get('/done', taskController.findAllDoneTasks)
+
+//Buscar una tarea por id
+router.get('/:id', taskController.findOneTask) 
+
+//borrar tarea
+router.delete('/:id', taskController.deleteTask)
+
+//actualizar tarea
+router.put('/:id', taskController.updateTask)
 
 //exportar
 export default router;
